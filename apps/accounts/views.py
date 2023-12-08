febe87f21c62dd5287c6f670b4b36619b87a8300
LangaMAd <artykovh@gmail.com>
2023-12-08 18:25:35 +0600

    add 0

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,21 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-# Амин лох
-
-# Adil
-
-#v_tymane
-#amfbskjdvksjdv
-
-#Amin popytka
-
-# adil top
-# hello
-
-
-# for ismar
-# НЕ ПУШ
-# взлом хасана
-# Кринге
-#  map metro v Log
