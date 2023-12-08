dd0c9598e23d039f5a7f6aa5b0531b9c1d269f6e
LangaMAd <artykovh@gmail.com>
2023-12-08 19:30:46 +0600

    add models

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,6 +1,71 @@
 from django.db import models
 
 # Create your models here.
-# ldijfosdhsu
-# ldijfosdhsu
-# v tymane
+
+
+
+
+from django.db import models
+
+# Create your models here.
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+MALE_CHOICES = (
+    ('Мужчина','Мужчина'),
+    ('Женщина','Женщина'),
+    ('Другое','Другое'),
+)
+
+class User(AbstractUser):
+    username = models.CharField('Имя пользователя',
+                                max_length=10,
+                                unique=True)
+    email = models.EmailField('Электронная почта',
+                              max_length=30,
+                              unique=True)
+    sex = models.CharField('Пол',
+                           choices=MALE_CHOICES,
+                           default='None')
+    age = models.DateTimeField('Дата рождения',
+                               null=True,
+                               blank=True)
+    phone = models.CharField('Телефон',
+                             max_length=10,
+                             null=True,
+                             blank=True)
+    created = models.DateTimeField('Дата создания аккаунта',
+                                   auto_now_add=True)
+    avatar = models.ImageField('фото',
+                               upload_to='media/images/',
+                               null=True,
+                               blank=True)
+
+    objects = UserManager()
--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5)
+
+
