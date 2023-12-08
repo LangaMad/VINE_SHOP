15fd5427c21ee0d10d8db3c521d9a2446a5fd50a
LangaMAd <artykovh@gmail.com>
2023-12-08 18:58:24 +0600

    add 0

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,14 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
