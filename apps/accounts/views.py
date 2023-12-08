69f0ec26e7d5f614517a56a938f5ca8bb4e633a7
LangaMAd <artykovh@gmail.com>
2023-12-08 17:31:36 +0600

    add ismar

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,16 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-# Амин лох
-
-# Adil
-
-#v_tymane
-# amfbskjdvksjdv
-
-#Amin popytka
-
-# adil top
-# hello
-#helloamin
-# browl Stars
