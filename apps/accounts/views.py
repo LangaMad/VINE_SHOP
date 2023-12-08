c807647ecfd8fc76e9efb8dc818fc470ed3fd1ae
Guma06 <k.g.a110406@gmail.com>
2023-12-08 18:02:02 +0600

    add hasan and ismar

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Амин лох
+
+# Adil
+
+#v_tymane
+# amfbskjdvksjdv
+
+#Amin popytka
+
+# adil top
+# hello
+
+
+
+
+
+# ismar krasawchik
+# a hasan ne top
