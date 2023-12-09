1c022d55bae94705ff7f16d3f5a78f7021cb3703
Erlan1441 <ee79334@gmail.com>
2023-12-09 16:42:36 +0600

    urls_product

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpattern = [
-
+    path('ProdutcListView', ProductListView.as_view(), name='ProductListView'),
+    path ('ProductDetailsView', ProductDetailView.as_view(), name='ProductDetailsView')
 ]
