64173da9d3149f956e7282da3fe047e4e2fda072
LangaMAd <artykovh@gmail.com>
2023-12-15 18:31:48 +0600

    add api

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('v1/list/', ProductApiViewset.as_view({'get':'list'}), name='ProductListView'),
+    path('v1/list1/', ProductApiViewset.as_view({'get':'list'}), name='ProductListView'),
     # path('Product/Details/View', ProductDetailView.as_view(), name='ProductDetailsView'),
     # path('v1/create/', ProductApiCreateView.as_view(), name='ProductApiCreateView'),
     path('v1/list/<int:pk>/', ProductApiViewset.as_view({'put':'update'}), name='ProductApiUpdateView'),
+    path('v1/list/',CustomProductApiView.as_view(),  name='CustomProductApiView'),
+    path('v1/list2/', ProductListAPIView.as_view(), name='CustomProductApiView2'),
 
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,10 +1,63 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
 from .models import Product
-from rest_framework import generics
+from rest_framework import generics , mixins
 from rest_framework import viewsets
 from .serializers import ProductSerializer
-# Create your views here.
+from rest_framework.response import Response
+
+
+
+class CustomProductApiView(mixins.ListModelMixin,GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+# class CustomProductApiView(APIView):
+#     def get(self, request):
+#         lst = Product.objects.all().values()
+#         return  Response(list(lst))
+#
+#     def post(self, request):
+#         post_data = Product.objects.create(
+#             name=request.data['name'],
+#             description=request.data['description'],
+#             price=request.data['price'],
+#             count=request.data['count'],
+#             public_date=request.data['public_date'],
+#             brand = request.data['brand'],
+#             photo=request.data['photo'],
+# )
+#         return Response({'post':model_to_dict(post_data)})
+# class ListAPIView(mixins.ListModelMixin,
+#                   GenericAPIView):
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class ProductApiViewset(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
@@ -22,13 +75,6 @@ class ProductApiCreateView(generics.CreateAPIView): # Создает обьек�
 class ProductApiUpdateView(generics.UpdateAPIView): # Обновляет обьект
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-
-
-
-
-
-
-
 
 class ProductDetailView(DetailView):
     model = Product
